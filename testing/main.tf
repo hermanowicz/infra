cec38c14617dd7e05be0529229f7cce133114352
Tomasz Hermanowicz <hermangti@gmail.com>
2025-01-03 22:26:31 +0100

    formatig ...

--- testing/main.tf
+++ testing/main.tf
@@ -22,7 +22,7 @@ resource "hcloud_network_subnet" "network-subnet" {
   ip_range     = "10.0.1.0/24"
 }
 
-resource "hcloud_server" "server" {
+resource "hcloud_server" "server-1" {
   name        = "pyinfra-1"
   server_type = "cx22"
   image       = "ubuntu-24.04"
@@ -35,13 +35,65 @@ resource "hcloud_server" "server" {
   network {
     network_id = hcloud_network.main-net.id
     ip         = "10.0.1.5"
-    alias_ips = [
-      "10.0.1.6",
-      "10.0.1.7"
-    ]
+    # alias_ips = [
+    #   "10.0.1.6",
+    #   "10.0.1.7"
+    # ]
   }
 
   depends_on = [
     hcloud_network_subnet.network-subnet
   ]
+}
+
+resource "hcloud_server" "server-2" {
+  name        = "pyinfra-2"
+  server_type = "cx22"
+  image       = "ubuntu-24.04"
+  location    = "fsn1"
+
+  ssh_keys = [
+    "hetzner_key"
+  ]
+
+  network {
+    network_id = hcloud_network.main-net.id
+    ip         = "10.0.1.6"
+  }
+
+  depends_on = [
+    hcloud_network_subnet.network-subnet
+  ]
+}
+
+resource "hcloud_server" "server-3" {
+  name        = "pyinfra-3"
+  server_type = "cx22"
+  image       = "ubuntu-24.04"
+  location    = "fsn1"
+
+  ssh_keys = [
+    "hetzner_key"
+  ]
+
+  network {
+    network_id = hcloud_network.main-net.id
+    ip         = "10.0.1.7"
+  }
+
+  depends_on = [
+    hcloud_network_subnet.network-subnet
+  ]
+}
+
+output "server-1-public-ip" {
+  value = hcloud_server.server-1.ipv4_address
+}
+
+output "server-2-public-ip" {
+  value = hcloud_server.server-2.ipv4_address
+}
+
+output "server-3-public-ip" {
+  value = hcloud_server.server-3.ipv4_address
 }
--- testing/setup.py
+++ testing/setup.py
@@ -1,3 +1,4 @@
+from pyinfra import host
 from pyinfra.operations import apt, server
 
 apt.update(
@@ -11,28 +12,27 @@ apt.upgrade(
 
 apt.packages(
     name="install packages like fail2ban, ufw, etc",
-    packages=["fail2ban", "ufw", "curl", "wget", "git", "python3-pip", "python3-venv", "python3-dev", "build-essential",
-              "libssl-dev",
-              "golang", "docker.io"],
+    packages=["fail2ban", "ufw", "unattended-upgrades",
+              "curl", "wget", "git", "python3-pip",
+              "python3-dev", "build-essential",
+              "libssl-dev"],
     update=True,
     latest=True
 )
 
 server.shell(
-    name="printing to tty hello,world",
-    commands=["echo 'hello, world'"]
+    name="Enable UFW",
+    commands=[
+        "ufw default deny incoming",
+        "ufw default allow outgoing",
+        "ufw allow ssh",
+        "ufw --force enable",
+    ],
 )
 
 server.service(
     name="enable fail2ban",
     service="fail2ban",
-    enabled=True,
-    running=True
-)
-
-server.service(
-    name="enabling docker",
-    service="docker",
     enabled=True,
     running=True
 )
